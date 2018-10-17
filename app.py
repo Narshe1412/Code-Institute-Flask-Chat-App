634cfb9e93dbefdc40aa9dfd2bfb4e876f86b065
Manuel <manuel1412@gmail.com>
2018-10-17 15:31:02 +0000

    Refactor views to use html templates and a for loop to display messages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 messages = []
@@ -8,24 +8,30 @@ messages = []
 def add_messages(username, message):
     """ Add messages to the message list """
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("[{}] {}: {}".format(now, username, message))
+    message_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(message_dict)
     
 
 def get_all_messages():
     """Get all of messages in the system and separate them by a BR tag"""
-    return "<br>".join(messages)
+    return messages
     
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """ Main page with instructions """
-    return "To send a message use /USERNAME/MESSAGE"
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+            user_list.writelines(request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")
     
     
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome {0} </h1><p> {1} </p>".format(username, get_all_messages())
+    message = get_all_messages()
+    return render_template("chat.html", username=username, chat_messages = message)
     
     
 @app.route('/<username>/<message>')
